97c6a3d2df94a68b1907b95d363a9cc20f68068a
Alberto Aresca <alberto.aresca@gmail.com>
2012-02-27 20:59:14 -0300

    fixed test

--- src/test/java/com/example/EncryptFileTestCase.java
+++ src/test/java/com/example/EncryptFileTestCase.java
@@ -1,15 +1,36 @@
 package com.example;
 
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.util.FileUtils;
 
 public class EncryptFileTestCase extends FunctionalTestCase {
-  protected String getConfigResources() {
-    return "mule-config.xml";
-  }
+	private File fileEnc;
 
-  @Test
-  public void largeFilesAfterEncryptedDoesNotGetDeleted() throws Exception {
-    Thread.sleep(300000);//5 minutes
-  }
+	protected String getConfigResources() {
+		return "mule-config.xml";
+	}
+
+	@Before
+	public void setUp() {
+		fileEnc = new File(
+				"target/tmp/largeFilesOutbound/ubuntu-10.04.3-desktop-amd64.iso.pgp");
+		FileUtils.deleteQuietly(fileEnc);
+	}
+
+	@Test
+	public void largeFilesAfterEncryptedDoesNotGetDeleted() throws Exception {
+		int i=0;
+		do {
+			Thread.sleep(300000);// 5 minutes
+			logger.info("Itreation #"+ i++ +" performed, time to get back to sleep");
+		} while (fileEnc.length() < 886596353);
+		logger.info("File correctly encrypted");
+		Thread.sleep(30000);
+		Assert.assertFalse(new File("target/tmp/largeFilesInbound/ubuntu-10.04.3-desktop-amd64.iso").exists());
+	}
 }
